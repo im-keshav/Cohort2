a9e115575af52557218d69e8dc9d59b9f832f7e6
im-keshav <mehtakeshav029@gmail.com>
2026-01-20 22:12:03 +0530

    "initial commit"

---
+++ backend/day-1/app.js
@@ -0,0 +1,11 @@
+// const cat = require('cat-me');
+
+
+// console.log(cat());
+
+const express = require('express');
+
+const app = express()// server ko create kiya
+
+app.listen(3000)// server ko start kiya
+
