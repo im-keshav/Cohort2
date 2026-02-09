b214244a522bb56e43a1036432c8725d7190d191
im-keshav <mehtakeshav029@gmail.com>
2026-02-09 14:39:57 +0530

    "initail commit"

---
+++ backend/day-3/server.js
@@ -0,0 +1,25 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.json())
+
+const notes=[]
+
+app.post("/notes",(req,res)=>{
+    // console.log(req.body);
+
+    notes.push(req.body)
+    
+    res.json({message:"Note created successfully", notes:req.body})
+})
+
+app.get("/notes",(req,res)=>{
+    res.json({message:"Notes retrieved successfully", notes:notes})
+})
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000")
+})
