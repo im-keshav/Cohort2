9aeeaa232974821aaaf3574b87aec5209bfdd965
im-keshav <mehtakeshav029@gmail.com>
2026-02-16 21:49:33 +0530

    "update"

--- backend/day-7/src/app.js
+++ backend/day-7/src/app.js
@@ -10,14 +10,14 @@ app.use(express.json())
 app.post("/notes", async (req,res)=>{
 
     const {title,description,age} = req.body
-    const note = await noteModel.create({
+    const notes = await noteModel.create({
         title, description,age
     })
 
 
     res.status(201).json({
         message:"Notes created successfully",
-        note 
+        notes
     })
 
 
@@ -26,12 +26,11 @@ app.post("/notes", async (req,res)=>{
 
 app.get("/notes",async (req,res)=>{
 
-    const note = await noteModel.find({
-        title , description,age
+    const notes = await noteModel.find({
     })
     res.status(200).json({
         messsage:"Notes fetched successfully",
-        note
+        notes
     })
 })
 
